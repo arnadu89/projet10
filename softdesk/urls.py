d9a7441f3492886331148358ae404429a71cbc8b
arnadu89 <arnadu89@gmail.com>
2023-02-18 17:18:05 +0100

    jwt auth install

---
+++ softdesk/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='obtain_token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+]
